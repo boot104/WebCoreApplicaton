86a6bff5cb001f84fccb1f3bf88f1fea37f2d73e
Victor Khabarov <victor.khabarov@custdata.com>
2018-06-13 11:29:51 -0400

    Commit 1

--- WebApplicationCore/Controllers/HomeController.cs
+++ WebApplicationCore/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WebApplicationCore.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            var t = "";
             return View();
         }
 
